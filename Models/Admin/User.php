4b0ec49d43f28af2325735718d2f60b97435bd74
enerjiser2 <project@idefclub.ru>
2015-11-01 22:50:09 +0300

    доделал метод setPassword()

--- Models/Admin/User.php
+++ Models/Admin/User.php
@@ -24,10 +24,14 @@ class User extends Model
     public function setPassword($password)
     {
         $validEmail = filter_var($this->email, FILTER_VALIDATE_EMAIL);
-
         if ($validEmail == true) {
+        // start 1* подумать как переделать это.
+            $tmp=$this->getRecordBy('email',$validEmail);
+            $this->id=$tmp->id;
+        // end 1*
             $this->hash = password_hash($password, PASSWORD_DEFAULT);
             $this->save();
+            var_dump($this);
             return true;
         }
         return false;
