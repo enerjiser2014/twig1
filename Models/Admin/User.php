93e9c388ad0975abfee5a0541a0832c307a0a991
enerjiser2 <project@idefclub.ru>
2015-11-01 21:14:47 +0300

    Установка пароля пользователя.

--- Models/Admin/User.php
+++ Models/Admin/User.php
@@ -23,7 +23,13 @@ class User extends Model
 
     public function setPassword($password)
     {
+        $validEmail = filter_var($this->email, FILTER_VALIDATE_EMAIL);
 
+        if ($validEmail == true) {
+            $this->hash = password_hash($password, PASSWORD_DEFAULT);
+            $this->save();
+        }
+        return false;
     }
 
     public function changePassword()
