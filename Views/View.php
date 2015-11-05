6b137b95edf74c0919e138e263966692fbe06747
enerjiser2 <project@idefclub.ru>
2015-11-05 13:45:29 +0300

    поменял расположение аргументов в методе display

--- Views/View.php
+++ Views/View.php
@@ -4,8 +4,9 @@ namespace App\Views;
 
 class View
 {
-    public static function display($m,$template)
+    public static function display($template,$m)
     {
+        var_dump($x);
         $loader = new \Twig_Loader_Filesystem(__DIR__ . '\..\templates');
         $twig = new \Twig_Environment($loader,
             [
